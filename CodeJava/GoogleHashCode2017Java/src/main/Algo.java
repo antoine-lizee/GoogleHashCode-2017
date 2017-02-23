bf2489c221af6ebbc45f413441902d35dc46cb29
emmanuelcharon <emmanuelcharon@hotmail.com>
2017-02-23 18:36:37 +0100

    main package and also class

---
+++ CodeJava/GoogleHashCode2017Java/src/main/Algo.java
@@ -0,0 +1,13 @@
+package main;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class Algo {
+
+	private static final Logger LOGGER = Logger.getLogger(Algo.class.getName());
+	
+	 public static void main(String[] args) throws IOException {
+	    	System.out.println("aaa");
+	 }
+}
