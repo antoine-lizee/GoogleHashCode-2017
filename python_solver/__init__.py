1773d5ad8b677af94a55d721f9665c9c45b1f16f
antoine.lizee <antoine.lizee@gmail.com>
2017-02-23 18:04:32 +0100

    enh: first python commit

---
+++ python_solver/__init__.py
@@ -0,0 +1,10 @@
+from pprint import pprint
+
+
+class Object:
+    def __init__(self, file_path):
+        with open(file_path) as f:
+            self.data = f.readlines()
+
+    def __repr__(self):
+        pprint(self.data)
